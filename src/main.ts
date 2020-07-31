70e795018f489b95c3981f5cb5191e01ca0d0788
huangkairan <18757128360@163.com>
2020-07-31 18:20:50 +0800

    first

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.scss'
+import  { createWebHashHistory ,createRouter } from 'vue-router'
+
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+
+const history = createWebHashHistory()
+const router = createRouter({
+  history,
+  routes:[
+    {
+    path:'/',component: Home
+  },
+  {
+    path:'/doc',component:Doc
+  }]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Topnav/>
+    <div class="banner">
+      <h1>KABA UI</h1>
+      <h2>Vue3 练习</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+<script lang='ts'>
+import Topnav from '../components/Topnav.vue'
+export default {
+  components:{Topnav}
+}
+</script>
+
+<style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
