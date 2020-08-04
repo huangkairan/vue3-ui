a98bfb6bb87706d90c8196e110a24331369036a6
huangkairan <18757128360@163.com>
2020-08-04 13:57:25 +0800

    doc默认

--- src/router.ts
+++ src/router.ts
@@ -12,6 +12,7 @@ export  const router = createRouter({
   {
     path:'/doc',component:()=>import('./views/Doc.vue'),
     children:[
+      {path:'',component:()=>import('./Components/DocDemo.vue')},
       {path:'switch',component:()=>import('./Components/SwitchDemo.vue')},
       {path:'button',component:()=>import('./Components/ButtonDemo.vue')},
       {path:'dialog',component:()=>import('./Components/DialogDemo.vue')},
