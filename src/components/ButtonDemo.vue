9e161a5fcddd599637db3880e9682520c7e07af4
huangkairan <18757128360@163.com>
2020-08-10 14:10:14 +0800

    css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-   <h1>demo 1</h1>
-   <Button @click="onClick"
-    @mouseover="onClick"
-    @focus="onClick"
-   >按钮</Button>
+    <h1>demo 1</h1>
+    <div>
+      <Button theme="button">按钮1</Button>
+      <Button theme="link">按钮2</Button>
+      <Button theme="text">按钮3</Button>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
-import Button from '../lib/Button.vue'
+import Button from "../lib/Button.vue";
 export default {
-  components:{
-    Button
+  components: {
+    Button,
   },
-  setup(){
-    const onClick=()=>{
-      console.log('hi')
-    }
-    return { onClick }
-  }
-}
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,22 @@
 <template>
-    <button>
+    <button class="kaba-btn" :class="`theme-${theme}`">
       <slot />
     </button>
 </template>
 <script lang='ts'>
 export default {
+  props:{
+    theme: {
+      type:String,
+      default:'button'
+    }
+  },
   setup(props,context){
 
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
 </style>
