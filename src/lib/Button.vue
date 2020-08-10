cda4a628c5abf6858e117470095d46fee1bcffce
huangkairan <18757128360@163.com>
2020-08-10 13:59:31 +0800

    button init 2

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,23 +1,15 @@
 <template>
-  <div :size ="size">
-    <button v-bind="rest">
+    <button>
       <slot />
     </button>
-  </div>
 </template>
 <script lang='ts'>
 export default {
-  inheritAttrs: false,
   setup(props,context){
-    const { size , ...rest } = context.attrs
 
-    return {size, rest }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  div{
-    border:1px solid black
-  }
 </style>
