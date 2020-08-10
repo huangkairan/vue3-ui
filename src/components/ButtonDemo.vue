9e4bfa6742e5e082bbdd109bc37d3f316598ec09
huangkairan <18757128360@163.com>
2020-08-10 13:58:27 +0800

    button init

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    button demo
+   <h1>demo 1</h1>
+   <Button @click="onClick"
+    @mouseover="onClick"
+    @focus="onClick"
+   >按钮</Button>
   </div>
 </template>
 
 <script lang='ts'>
+import Button from '../lib/Button.vue'
 export default {
-
+  components:{
+    Button
+  },
+  setup(){
+    const onClick=()=>{
+      console.log('hi')
+    }
+    return { onClick }
+  }
 }
 </script>
 
