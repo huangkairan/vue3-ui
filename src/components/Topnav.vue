db3718900a3f5151440175a25dc0f22a9609c678
huangkairan <18757128360@163.com>
2020-09-12 20:02:07 +0800

    css修改

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,33 +1,50 @@
 <template>
-  <div>
-    <div class="topnav">
-      <div class="logo" >LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-      <span class="toggleAside" @click="toggleAside">蛤</span>
-    </div>
-  </div>
+<div class="topnav">
+  <router-link to="/" class="logo">
+    <svg class="icon">
+      <use xlink:href="#icon-huang"></use>
+    </svg>
+  </router-link>
+  <ul class="menu">
+    <li>
+      <router-link to="/doc">文档</router-link>
+    </li>
+  </ul>
+  <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+    <use xlink:href="#icon-menu"></use>
+  </svg>
+</div>
 </template>
 
-<script lang='ts'>
-import { inject, Ref } from 'vue'
+<script lang="ts">
+import {
+  inject,
+  Ref
+} from "vue";
 export default {
-  setup(){
-    const asideVisible = inject<Ref<boolean>>('asideVisible')
-    // console.log(asideVisible.value)
-    const toggleAside = ()=>{
-      asideVisible.value = !asideVisible.value
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false
     }
-    return { toggleAside }
-  }
-}
+  },
+  setup() {
+    const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return {
+      toggleMenu
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
+
 .topnav {
-  background: pink;
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -36,37 +53,49 @@ export default {
   width: 100%;
   z-index: 10;
   justify-content: center;
-  align-content: center;
-  > .logo {
+  align-items: center;
+
+  >.logo {
     max-width: 6em;
     margin-right: auto;
+
+    >svg {
+      width: 32px;
+      height: 32px;
+    }
   }
-  > .menu {
+
+  >.menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-    > li {
+
+    >li {
       margin: 0 1em;
     }
   }
+
   >.toggleAside {
-    display: none;
-    width:24px;
-    height: 24px;
-    background-color: light gray;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
+    display: none;
+    background: fade-out(black, 0.9);
   }
-  @media (max-width:500px) {
-    >.menu{
+
+  @media (max-width: 500px) {
+    >.menu {
       display: none;
     }
-    >.logo{
+
+    >.logo {
       margin: 0 auto;
     }
-    >.toggleAside{
+
+    >.toggleAside {
       display: inline-block;
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import './lib/kaba.scss'
 import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import { router } from './router';
-
+import './lib/svg.js'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
